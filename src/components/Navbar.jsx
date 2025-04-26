044f6b6c0c241f983af2b6d91c6b7d4cdb12ef6b
Hiren Khanchandani <hirenkhanchandani14@gmail.com>
2025-04-25 20:36:50 -0400

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -96,7 +96,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://www.linkedin.com/in/hiren-khanchandani1407'
+              href='https://www.linkedin.com/in/hiren-khanchandani'
             >
               Linkedin <FaLinkedin size={30} />
             </a>
